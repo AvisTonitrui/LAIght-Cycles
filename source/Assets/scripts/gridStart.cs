a88de74681bdae68e40b7bc886a15f359538c8f0
AvisTonitrui <bugaboo421@gmail.com>
2018-02-20 15:09:43 -0800

    Now fill the grid with trails.

---
+++ source/Assets/scripts/gridStart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gridStart : MonoBehaviour {
+
+    public GameObject trail;
+    private const int gridSize = 44;
+
+
+	// Use this for initialization
+	void Start () {
+		for (int x = -gridSize; x <= gridSize; x++) {
+            for (int y = -gridSize; y <= gridSize; y++) {
+                GameObject thisTrail = Instantiate(trail, new Vector3(x, y, 0), Quaternion.identity);
+                thisTrail.SetActive(false);
+            }
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
