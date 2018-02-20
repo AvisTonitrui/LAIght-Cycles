87a7133b020a626f8499542393f1e68e13b4a3c9
AvisTonitrui <bugaboo421@gmail.com>
2018-02-20 14:54:56 -0800

    Cycles will now always move forward.

--- source/Assets/scripts/cycles/cycleMovement.cs
+++ source/Assets/scripts/cycles/cycleMovement.cs
@@ -6,24 +6,33 @@ public class cycleMovement : MonoBehaviour {
     //This class is for moving the cycles
 
     public int direction; //This is an int which determines which direction the cycle will go
-    private const int speed = 5; //The speed of the cycle, which is constant but will need testing
+    private const float speed = 0.25f; //The speed of the cycle, which is constant but will need testing
     public GameObject cycle;
+    public Object trail;
     
-    private void turn(int face) {
+    private void turn(int face) { // turns the cycle to face up, down, left, or right (1, 2, 3, 4 respectively)
         if (face == 1) {
-            cycle.transform.eulerAngles = new Vector3(0, 0, 90);
+            cycle.transform.eulerAngles = new Vector3(0, 0, 90); //turn up
         }else if (face == 2) {
-
+            cycle.transform.eulerAngles = new Vector3(0, 0, 270); //turn down
+        }else if (face == 3) {
+            cycle.transform.eulerAngles = new Vector3(0, 0, 180); //turn left
+        }else if (face == 4) {
+            cycle.transform.eulerAngles = new Vector3(0, 0, 0); //turn right
         }
     }
 
 	// Use this for initialization
 	void Start () {
-        turn(1);
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        cycle.transform.position = Vector3.MoveTowards(cycle.transform.position, cycle.transform.position + cycle.transform.right, speed); //constantly moves the cycle forward
+
+        if (cycle.transform.position.x % 1 == 0 && cycle.transform.position.y % 1 == 0) { //checks if cycle is on a grid point
+            
+        }
 	}
 }
