a5996ec510b12f8ad42e00232c01c8117c63cafb
AvisTonitrui <bugaboo421@gmail.com>
2018-02-23 14:44:38 -0800

    Fixed cycles going off the grid system.

--- source/Assets/scripts/cycles/cycleMovement.cs
+++ source/Assets/scripts/cycles/cycleMovement.cs
@@ -15,12 +15,18 @@ public class cycleMovement : MonoBehaviour {
     private void turn(int face) { // turns the cycle to face up, down, left, or right (1, 2, 3, 4 respectively)
         if (face == 1) {
             cycle.transform.eulerAngles = new Vector3(0, 0, 90); //turn up
+            cycle.transform.position = new Vector3(Mathf.Round(cycle.transform.position.x), cycle.transform.position.y, 0);
         }else if (face == 2) {
             cycle.transform.eulerAngles = new Vector3(0, 0, 270); //turn down
-        }else if (face == 3) {
+            cycle.transform.position = new Vector3(Mathf.Round(cycle.transform.position.x), cycle.transform.position.y, 0);
+        }
+        else if (face == 3) {
             cycle.transform.eulerAngles = new Vector3(0, 0, 180); //turn left
-        }else if (face == 4) {
+            cycle.transform.position = new Vector3(cycle.transform.position.x, Mathf.Round(cycle.transform.position.y), 0);
+        }
+        else if (face == 4) {
             cycle.transform.eulerAngles = new Vector3(0, 0, 0); //turn right
+            cycle.transform.position = new Vector3(cycle.transform.position.x, Mathf.Round(cycle.transform.position.y), 0);
         }
     }
 
@@ -35,15 +41,29 @@ public class cycleMovement : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
+        //Keeps the cycle moving
         if (processing.GetComponent<background>().gameActive == true) {
             cycle.transform.position = Vector3.MoveTowards(cycle.transform.position, cycle.transform.position + cycle.transform.right, speed); //Constantly moves the cycle forward
         }
-        
+
+        //Corrects floating Point errors
+        if (Mathf.Abs(cycle.transform.position.x) < 0.01) {
+            cycle.transform.position = new Vector3(0, cycle.transform.position.y, 0);
+        }
+
+        if (Mathf.Abs(cycle.transform.position.y) < 0.01) {
+            cycle.transform.position = new Vector3(cycle.transform.position.x, 0, 0);
+        }
+
+        //turns cycle, creates trails, and ensures the next grid spot it's going to is legal
         if (Mathf.Approximately(cycle.transform.position.x % 1, 0) && Mathf.Approximately(cycle.transform.position.y % 1, 0)) { //Checks if cycle is on a grid point
             trail.GetComponent<SpriteRenderer>().sprite = color; //Changes the trail to the proper color
             trail.GetComponent<Renderer>().enabled = true; //Enables the trail
             turn(direction); //Turns the cycle in the last direction it was given
-            Debug.Log("Hit a grid point!");
+
+            //These are the controls to make sure that the square the cycle is heading into is legal
         }
+
+        
 	}
 }
