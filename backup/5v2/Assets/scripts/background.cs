975d33b8e22b87ba2ebfc2f5d98a6495fcd8712c
AvisTonitrui <bugaboo421@gmail.com>
2018-03-05 13:53:37 -0800

    Added another version of the backup for safekeeping.

---
+++ backup/5v2/Assets/scripts/background.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class background : MonoBehaviour {
+
+    public bool gameActive = true; //Variable that states whether the game is going or not
+    public int victor = 0; //The victor: 0 means no victor yet, 1 and 2 to respective players, 3 is a tie
+    public bool player1IsHuman, player2IsHuman, training; //whether certain players are human and whether or not in training
+    public GameObject player1, player2; //The players
+
+    // Use this for initialization
+    void Start() {
+        if (player1IsHuman) {
+            player1.GetComponent<AIControl>().enabled = false;
+        }
+        else {
+            player1.GetComponent<humanControl>().enabled = false;
+        }
+
+        if (player2IsHuman) {
+            player2.GetComponent<AIControl>().enabled = false;
+        }
+        else {
+            player2.GetComponent<humanControl>().enabled = false;
+        }
+
+        if (player1IsHuman && player2IsHuman) {
+            training = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //Debug.Log(victor);
+        if (victor > 0) {
+            gameActive = false;
+
+            if ((player1IsHuman || player2IsHuman)) {
+                if (victor == 1) {
+                    SceneManager.LoadScene("player 1 win");
+                }
+                else if (victor == 2) {
+                    SceneManager.LoadScene("player 2 win");
+                }
+                else {
+                    SceneManager.LoadScene("tie");
+                }
+            }
+        }
+    }
+}
