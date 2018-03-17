e734c5e0b70f0706e43171bf36f4680b2b78ede9
AvisTonitrui <bugaboo421@gmail.com>
2018-03-17 12:47:34 -0700

    Added a backup folder so I can work at home.

---
+++ backup/3-17-18/Assets/scripts/fileWriteTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class fileWriteTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        try {
+
+            //Pass the filepath and filename to the StreamWriter Constructor
+            StreamWriter sw = new StreamWriter("Saves/MyFile.txt");
+
+            //Write a line of text
+            sw.WriteLine("Hello World!!");
+
+            //Write a second line of text
+            sw.WriteLine("From the StreamWriter class");
+
+            //Close the file
+            sw.Close();
+        }
+        catch (Exception e) {
+            Debug.Log("Exception: " + e.Message);
+        }
+        finally {
+            Debug.Log("Executing finally block.");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
